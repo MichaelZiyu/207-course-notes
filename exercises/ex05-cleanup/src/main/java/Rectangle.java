4a51ddb3e073eea98768c387765f79341a2f2122
MichaelZiyu <michaelziyu.zhou@mail.utoronto.ca>
2026-09-22 16:09:08 -0400

    ex05 fixed

--- exercises/ex05-cleanup/src/main/java/Rectangle.java
+++ exercises/ex05-cleanup/src/main/java/Rectangle.java
@@ -1,45 +1,50 @@
 //This file defines a Rectangle class to represent a rectangle.
 
-
+/**
+ * Rectangle class represents a rectangle, with some methods helping to describe the this rectangle
+ * Attributes:
+ *  private double width: the width of the rectangle
+ *  private double height: the height of the rectangle
+ */
 public class Rectangle {
-    //Rectangle class represents a rectangle, with some methods giving more ways to describe this rectangle
-    // Attributes:
-        // private double width: the width of the rectangle
-        // private double height: the height of the rectangle
+  private double width;
+  private double height;
 
-
-    private double width;
-    private double height;
-
-    //initialize the rectangle by its width and height
-    // @param w, h
-    public Rectangle(double w,double h){
+    /**initialize the rectangle by its width and height
+    * @param w: width
+    * @param h: height
+    */
+  public Rectangle(double w,double h){
         this.width=w;
         this.height=h;
-    }
+  }
 
 
-    public double area(){
+    /** calculate and return width times height
+     *
+     * @return width times height
+     */
+  public double area(){
         return width*height;
-    }
+  }
 
     /**
      * scales the rectangle
-     * @param factor
+     * @param factor scalling factor
      */
-    public void scale(double factor) {
+  public void scale(double factor) {
       width = width * factor;
       height = height * factor;
-    }
+  }
 
     /**
      * compare the rectangle with another rectangle by areas
-     * @param other
+     * @param item another rectangle
      */
-    public boolean isLargerThan(Rectangle other){
-        if(area() > other.area())
+  public boolean isLargerThan(Rectangle item){
+        if(area() > item.area())
             return true;
         else
             return false;
-    }
+  }
 }
