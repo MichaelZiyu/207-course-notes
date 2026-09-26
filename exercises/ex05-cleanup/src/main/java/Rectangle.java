ca4f918adf44e1db36480d2f136b973d8d75e2db
MichaelZiyu <michaelziyu.zhou@mail.utoronto.ca>
2026-09-26 11:52:39 -0400

    ex05 fixed

--- exercises/ex05-cleanup/src/main/java/Rectangle.java
+++ exercises/ex05-cleanup/src/main/java/Rectangle.java
@@ -1,4 +1,3 @@
-//This file defines a Rectangle class to represent a rectangle.
 
 /**
  * Rectangle class represents a rectangle, with some methods helping to describe the this rectangle
@@ -10,41 +9,41 @@ public class Rectangle {
   private double width;
   private double height;
 
-    /**initialize the rectangle by its width and height
-    * @param w: width
-    * @param h: height
-    */
+  /**initialize the rectangle by its width and height
+   * @param w: width
+   * @param h: height
+  */
   public Rectangle(double w,double h){
-        this.width=w;
-        this.height=h;
+    this.width=w;
+    this.height=h;
   }
 
 
-    /** calculate and return width times height
-     *
-     * @return width times height
-     */
+  /** calculate and return width times height
+   * @return width times height
+  */
   public double area(){
         return width*height;
   }
 
-    /**
-     * scales the rectangle
-     * @param factor scalling factor
-     */
+  /**
+   * scales the rectangle
+   * @param factor scalling factor
+  */
   public void scale(double factor) {
-      width = width * factor;
-      height = height * factor;
+    width = width * factor;
+    height = height * factor;
   }
 
-    /**
-     * compare the rectangle with another rectangle by areas
-     * @param item another rectangle
-     */
+  /**
+   * compare the rectangle with another rectangle by areas
+   * @param item another rectangle
+   * @return whether current area is larger than item area
+  */
   public boolean isLargerThan(Rectangle item){
-        if(area() > item.area())
-            return true;
-        else
-            return false;
+    if(area() > item.area())
+      return true;
+    else
+      return false;
   }
 }
